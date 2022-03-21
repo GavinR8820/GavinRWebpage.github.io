293dca13914cc43674416664f285b51e13ac6716
GavinR8820 <102012125+GavinR8820@users.noreply.github.com>
2022-03-21 03:15:28 -0400

    Add files via upload

---
+++ Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gavin Rintoul Assignment 1 Part 2");
+
+            double f, c;
+
+            Console.WriteLine("Enter The Value Of Celcsius:");
+            c = Convert.ToDouble(Console.ReadLine());
+            f = c * 1.8 + 32;
+            Console.WriteLine("Gavin Rintoul");
+            Console.WriteLine("Grintoul8820@conestoga.on.ca");
+            Console.WriteLine(c + "°C in Fahrenheit is: " + f + "°F");
+            Console.WriteLine("Moderate");
+            Console.WriteLine("Country: Canada");
+
+
+
+
+
+        }
+    }
+}
